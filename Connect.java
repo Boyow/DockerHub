e54c10e87dc311fb173d7d22fd05d1d839a15510
Marcin <46182860+Boyow@users.noreply.github.com>
2020-11-25 11:28:37 +0100

    Update Connect.java

--- Connect.java
+++ Connect.java
@@ -12,51 +12,26 @@ public class Connect {
     static final String USER = "mkozicki";
     static final String PASS = "mkozicki";
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Statement stmt = null;
-        while (true) {
-            try {
-                Class.forName("com.mysql.jdbc.Driver");
+    static Statement stmt = null;
+    static Connection conn = null;
 
-                System.out.println("Connecting to database...");
-                Connection conn = (Connection) DriverManager.getConnection(DB_URL, USER, PASS);
-
-                stmt = conn.createStatement();
-                String sql;
-                sql = "CREATE DATABASE IF NOT EXISTS `pfwco_lab4`;\n"
-                        + "CREATE TABLE `pfwco_lab4`.`pracownik` ( \n"
-                        + "`id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, \n"
-                        + "`nazwisko` VARCHAR(50) NOT NULL , \n"
-                        + "`pensja` VARCHAR(10) NOT NULL , \n"
-                        + "`firma` VARCHAR(400) NOT NULL\n"
-                        + "); \n"
-                        + "INSERT INTO `pfwco_lab4`.`pracownik`(nazwisko, pensja, firma) VALUES \n"
-                        + "(\"Cichy\", \"3\", \"Goudex\"),\n"
-                        + "(\"Marcinek\", \"333\", \"testowa\"),\n"
-                        + "(\"Komar\",\"23\",\"Dostra\"),\n"
-                        + "(\"Wesoły\",\"52\",\"Kwantowa\");";
-                stmt.execute(sql);
-                sql = "SELECT * FROM pfwco_lab4`.`pracownik";
-                ResultSet rs = stmt.executeQuery(sql);
-
-                while (rs.next()) {
-                    int id = rs.getInt("id");
-                    String nazwisko = rs.getString("nazwisko");
-                    String pensja = rs.getString("pensja");
-                    String firma = rs.getString("firma");
-
-                    System.out.println("ID: " + id);
-                    System.out.println(", nazwisko: " + nazwisko);
-                    System.out.println(", pensja: " + pensja);
-                    System.out.println(", firma: " + firma);
-                }
-                rs.close();
-                stmt.close();
-                conn.close();
-            } catch (SQLException | ClassNotFoundException se) {
-                se.printStackTrace();
-            }
-        }
+    public static void main(String[] args) throws SQLException {
+        Connect c = new Connect();
+        c.openConnection();
     }
-}
 
+    public Connection openConnection() throws SQLException {
+        if (conn == null) {
+            try {
+                    Class.forName(JDBC_DRIVER);
+                    conn = (Connection) DriverManager.getConnection(DB_URL, USER, PASS);
+                    System.out.println("CONNECTION SUCCESSFUL");
+                } catch (ClassNotFoundException ex) {
+                    Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+                    System.out.println("CONNECTION ERROR");
+                }
+        }
+        return conn;
+    }
+
+}
