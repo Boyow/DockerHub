1d6597fbc6d58b43bd9675e3a0c13dee052c7273
Marcin <46182860+Boyow@users.noreply.github.com>
2020-11-25 10:13:57 +0100

    Update Connect.java

--- Connect.java
+++ Connect.java
@@ -12,35 +12,51 @@ public class Connect {
     static final String USER = "mkozicki";
     static final String PASS = "mkozicki";
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException{
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         Statement stmt = null;
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
+        while (true) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
 
-            System.out.println("Connecting to database...");
-            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                System.out.println("Connecting to database...");
+                Connection conn = (Connection) DriverManager.getConnection(DB_URL, USER, PASS);
 
-            stmt = conn.createStatement();
-            String sql;
-            sql = "SELECT * FROM pracownik";
-            ResultSet rs = stmt.executeQuery(sql);
+                stmt = conn.createStatement();
+                String sql;
+                sql = "CREATE DATABASE IF NOT EXISTS `pfwco_lab4`;\n"
+                        + "CREATE TABLE `pfwco_lab4`.`pracownik` ( \n"
+                        + "`id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, \n"
+                        + "`nazwisko` VARCHAR(50) NOT NULL , \n"
+                        + "`pensja` VARCHAR(10) NOT NULL , \n"
+                        + "`firma` VARCHAR(400) NOT NULL\n"
+                        + "); \n"
+                        + "INSERT INTO `pfwco_lab4`.`pracownik`(nazwisko, pensja, firma) VALUES \n"
+                        + "(\"Cichy\", \"3\", \"Goudex\"),\n"
+                        + "(\"Marcinek\", \"333\", \"testowa\"),\n"
+                        + "(\"Komar\",\"23\",\"Dostra\"),\n"
+                        + "(\"Wesoły\",\"52\",\"Kwantowa\");";
+                stmt.execute(sql);
+                sql = "SELECT * FROM pracownik";
+                ResultSet rs = stmt.executeQuery(sql);
 
-            while (rs.next()) {
-                int id = rs.getInt("id");
-                String nazwisko = rs.getString("nazwisko");
-                String pensja = rs.getString("pensja");
-                String firma = rs.getString("firma");
+                while (rs.next()) {
+                    int id = rs.getInt("id");
+                    String nazwisko = rs.getString("nazwisko");
+                    String pensja = rs.getString("pensja");
+                    String firma = rs.getString("firma");
 
-                System.out.println("ID: " + id);
-                System.out.println(", nazwisko: " + nazwisko);
-                System.out.println(", pensja: " + pensja);
-                System.out.println(", firma: " + firma);
+                    System.out.println("ID: " + id);
+                    System.out.println(", nazwisko: " + nazwisko);
+                    System.out.println(", pensja: " + pensja);
+                    System.out.println(", firma: " + firma);
+                }
+                rs.close();
+                stmt.close();
+                conn.close();
+            } catch (SQLException | ClassNotFoundException se) {
+                se.printStackTrace();
             }
-            rs.close();
-            stmt.close();
-            conn.close();
-        } catch (SQLException | ClassNotFoundException se) {
-            se.printStackTrace();
         }
     }
 }
+
